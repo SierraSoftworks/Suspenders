602d0f0f77744888e5947350dfe0a585645ec622
Benjamin Pannell <admin@sierrasoftworks.com>
2014-04-10 18:07:42 +0200

    Added commenting and minified JS

--- test/Server.js
+++ test/Server.js
@@ -27,9 +27,25 @@ describe('server', function() {
     it('should send the correct packets for a two-way call', function(done) {
         clientSocket.once('data', function(data) {
             JSON.parse(data).should.eql({ c: 'test', a: [1, 2, '3'], r: Object.keys(suspender.handleOnce)[0] });
+            suspender.handleOnce = {};
             done();
         });
 
         suspender.call('test', 1, 2, '3', function() { });
+    });
+    
+    it('should correctly handle callback functions', function(done) {
+        clientSocket.once('data', function(data) {
+            var returnFunction = Object.keys(suspender.handleOnce)[0];
+            JSON.parse(data).should.eql({ c: 'test', a: [1, 2, '3'], r: returnFunction });
+            clientSocket.write(JSON.stringify({ c: returnFunction, a: ['1', 2] }));
+        });
+
+        suspender.call('test', 1, 2, '3', function(a1, a2) {
+            Object.keys(suspender.handleOnce).length.should.equal(0);
+            a1.should.equal('1');
+            a2.should.equal(2);
+            done();    
+        });
     });
 });
